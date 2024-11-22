9b02a92902f367219767d86e374b31666cb16567
hbykilop <hbykilop@gmail.com>
2024-11-22 18:13:29 +0000

    An in-memory reliable byte stream

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,65 +1,75 @@
 #include "byte_stream.hh"
+#include <stdexcept>
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ), error_( false ), buf_() {}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+  return closed_;
 }
 
 void Writer::push( string data )
 {
-  // Your code here.
-  (void)data;
-  return;
+  if ( closed_ || error_ )
+    return;
+
+  uint64_t writable = min( available_capacity(), static_cast<uint64_t>( data.size() ) );
+  for ( uint64_t i = 0; i < writable; i++ ) {
+    buf_.push_back( data[i] );
+  }
+  bytes_pushed_ += writable;
+}
+
+void Writer::push( char ch )
+{
+  std::string str( 1, ch );
+  push( str );
 }
 
 void Writer::close()
 {
-  // Your code here.
+  closed_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+  return capacity_ - buf_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+  return bytes_pushed_;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return {};
+  return closed_ && buf_.empty();
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+  return bytes_popped_;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  return { std::string_view( &buf_.front(), 1 ) };
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  (void)len;
+  if ( len > bytes_buffered() ) {
+    throw std::runtime_error( "pop len larger than buffer contain" );
+  }
+  for ( uint64_t i = 0; i < len; i++ ) {
+    buf_.pop_front();
+  }
+  bytes_popped_ += len;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+  return buf_.size();
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <deque>
 #include <string>
 #include <string_view>
 
@@ -25,13 +26,19 @@ protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
   uint64_t capacity_;
   bool error_ {};
+
+  std::deque<char> buf_;
+  bool closed_ {};
+  uint64_t bytes_pushed_ = 0;
+  uint64_t bytes_popped_ = 0;
 };
 
 class Writer : public ByteStream
 {
 public:
   void push( std::string data ); // Push data to stream, but only as much as available capacity allows.
-  void close();                  // Signal that the stream has reached its ending. Nothing more will be written.
+  void push( char ch );
+  void close(); // Signal that the stream has reached its ending. Nothing more will be written.
 
   bool is_closed() const;              // Has the stream been closed?
   uint64_t available_capacity() const; // How many bytes can be pushed to the stream right now?
